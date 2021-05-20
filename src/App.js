8b96f6f79749764b24b1a05a89307501bbc0b09a
akshaj65 <jhonsonrozario13@gmail.com>
2021-05-20 14:22:45 +0530

    Basic ToDo App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,60 @@
+import {useState} from 'react'
+import './App.css';
+
 function App() {
+  const [toDos, setToDos] = useState([])
+  const [toDo, setToDo] = useState('')
   return (
-    <h1>Hello World</h1>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input value={toDo} onChange={(e)=> setToDo(e.target.value)}  type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=>setToDos([...toDos,{id: Date.now() , text: toDo ,status:false}])}  className="fas fa-plus"></i> {/*spread operator*/}
+      </div>
+      <div className="todos">
+      { toDos.map((obj)=>{
+        
+        return ( <div className="todo">
+                    <div className="left">
+                       <input onChange={(e)=>{
+                         console.log(e.target.value)
+                         console.log(obj)
+                         //filter creates a new array
+                        setToDos(toDos.filter(obj2=>{
+                            if(obj2.id===obj.id)
+                            {
+                              obj2.status = e.target.checked
+                            }
+                            return obj2
+                        }))
+                       }}
+                        value={obj.status} type="checkbox" name="" id="" />
+                       <p>{obj.text}</p>
+                    </div>
+                    <div className="right">
+                       <i onClick={()=> setToDos(toDos.filter(obj3=>obj3.id!==obj.id))} className="fas fa-times"></i>
+                    </div>
+                 </div> )
+        })}
+
+        {/*map function always expects a return */}
+        {toDos.map((obj)=>{
+
+            if(obj.status){
+                return(<h1>{obj.text}</h1>)
+            }
+            return null
+
+        })}
+
+      </div>
+    </div>
   );
 }
 
